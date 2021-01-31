4ea2b9ced878cb4605636fde80407fe1ab71b26e
onur-YASA <n.onur172@gmail.com>
2021-01-31 18:52:42 +0300

    Add project files.

---
+++ Ticari_Otomasyon/Frm Musteriler.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Ticari_Otomasyon
+{
+    public partial class Frm_Musteriler : Form
+    {
+        public Frm_Musteriler()
+        {
+            InitializeComponent();
+        }
+        sqlbaglantisi bgl = new sqlbaglantisi();
+        void listele()
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select*From TBL_MUSTERILER",bgl.baglanti());
+            da.Fill(dt);
+            gridControl1.DataSource = dt;
+        }
+        void sehirListesi()
+        {
+            SqlCommand komut = new SqlCommand("Select SEHIR From TBL_ILLER", bgl.baglanti());
+            SqlDataReader dr = komut.ExecuteReader();
+            while(dr.Read())
+            {
+                Cmbil.Properties.Items.Add(dr[0]);
+            }
+            bgl.baglanti().Close();
+        }
+
+        private void groupControl1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Frm_Musteriler_Load(object sender, EventArgs e)
+        {
+            listele();
+            sehirListesi();
+        }
+
+        private void Cmbil_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Cmbilce.Properties.Items.Clear();
+            SqlCommand komut = new SqlCommand("Select ILCE From TBL_ILCELER Where SEHIR=@p1",bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1",Cmbil.SelectedIndex+1);
+            SqlDataReader dr = komut.ExecuteReader();
+            while(dr.Read())
+            {
+                Cmbilce.Properties.Items.Add(dr[0]);
+
+            }
+            bgl.baglanti().Close();
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("insert into TBL_MUSTERILER(AD,SOYAD,TELEFON,TELEFON2,TC,MAIL,IL,ILCE,VERGIDAIRE,ADRES)values(@p1,@p2,@p3,@p4,@p5,@p6,@p7,@p8,@p9,@p10)", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", TxtAd.Text);
+            komut.Parameters.AddWithValue("@p2", TxtSoyad.Text);
+            komut.Parameters.AddWithValue("@p3", MskTelefon1.Text);
+            komut.Parameters.AddWithValue("@p4", MskTelefon2.Text);
+            komut.Parameters.AddWithValue("@p5", MskTC.Text);
+            komut.Parameters.AddWithValue("@p6", TxtMail.Text);
+            komut.Parameters.AddWithValue("@p7", Cmbil.Text);
+            komut.Parameters.AddWithValue("@p8", Cmbilce.Text);
+            komut.Parameters.AddWithValue("@p9", TxtVergi.Text);
+            komut.Parameters.AddWithValue("@p10", RchAdres.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Müşteri Sisteme Eklendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listele();
+        }
+
+        private void gridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+
+        }
+
+        private void gridControl1_Click(object sender, EventArgs e)
+        {
+            DataRow dr = gridView1.GetDataRow(gridView1.FocusedRowHandle);
+            if (dr != null)
+            {
+                Txtid.Text = dr["ID"].ToString();
+                TxtAd.Text = dr["AD"].ToString();
+                TxtSoyad.Text = dr["SOYAD"].ToString();
+                MskTelefon1.Text = dr["TELEFON"].ToString();
+                MskTelefon2.Text = dr["TELEFON2"].ToString();
+                MskTC.Text = dr["TC"].ToString();
+                TxtMail.Text = dr["MAIL"].ToString();
+                Cmbil.Text = dr["IL"].ToString();
+                Cmbilce.Text = dr["ILCE"].ToString();
+                TxtVergi.Text = dr["VERGIDAIRE"].ToString();
+                RchAdres.Text = dr["ADRES"].ToString();
+            }
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            SqlCommand komutsil = new SqlCommand("Delete From TBL_MUSTERILER where ID=@p1", bgl.baglanti());
+            komutsil.Parameters.AddWithValue("@p1", Txtid.Text);
+            komutsil.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Müşteri silindi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            listele();
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("update Tbl_MUSTERILER set AD=@p1,SOYAD=@p2,TELEFON=@p3,TELEFON2=@p4,TC=@p5,MAIL=@p6,IL=@p7,ILCE=@p8,VERGIDAIRE=@p9,ADRES=@p10 where ID=@p11", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", TxtAd.Text);
+            komut.Parameters.AddWithValue("@p2", TxtSoyad.Text);
+            komut.Parameters.AddWithValue("@p3", MskTelefon1.Text);
+            komut.Parameters.AddWithValue("@p4", MskTelefon2.Text);
+            komut.Parameters.AddWithValue("@p5", MskTC.Text);
+            komut.Parameters.AddWithValue("@p6", TxtMail.Text);
+            komut.Parameters.AddWithValue("@p7", Cmbil.Text);
+            komut.Parameters.AddWithValue("@p8", Cmbilce.Text);
+            komut.Parameters.AddWithValue("@p9", TxtVergi.Text);
+            komut.Parameters.AddWithValue("@p10", RchAdres.Text);
+            komut.Parameters.AddWithValue("@p11", Txtid.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Müşteri Bilgileri Güncellendi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listele(); komut.Parameters.AddWithValue("@p1", TxtAd.Text);
+            
+        }
+    }
+}
---
+++ Ticari_Otomasyon/FrmFirmalar.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Ticari_Otomasyon
+{
+    public partial class FrmFirmalar : Form
+    {
+        public FrmFirmalar()
+        {
+            InitializeComponent();
+        }
+        sqlbaglantisi bgl = new sqlbaglantisi();
+        void firmalistesi()
+        {
+
+            SqlDataAdapter da = new SqlDataAdapter("Select*From TBL_FIRMALAR", bgl.baglanti());
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gridControl1.DataSource = dt;
+        }
+        void sehirListesi()
+        {
+            SqlCommand komut = new SqlCommand("Select SEHIR From TBL_ILLER", bgl.baglanti());
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                Cmbil.Properties.Items.Add(dr[0]);
+            }
+            bgl.baglanti().Close();
+        }
+        void carikodaciklamalar()
+        {
+            SqlCommand komut = new SqlCommand("Select FIRMAKOD1 from TBL_KODLAR",bgl.baglanti());
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                RchKod1.Text = dr[0].ToString();
+            }
+            bgl.baglanti().Close();
+        }
+        private void gridControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtAd_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textEdit1_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void xtraTabPage4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Txtid_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void temizle()
+        {
+            TxtAd.Text = "";
+            Txtid.Text = "";
+            TxtKod1.Text = "";
+            TxtKod2.Text = "";
+            TxtKod3.Text = "";
+            TxtSektor.Text = "";
+            TxtVergi.Text = "";
+            TxtYetkili.Text = "";
+            TxtYetkiliGorev.Text = "";
+            MskFax.Text = "";
+            MskTelefon1.Text = "";
+            MskTelefon2.Text = "";
+            MskTelefon3.Text = "";
+            MskYetkiliTC.Text = "";
+            TxtMail.Text = "";
+            RchAdres.Text = "";
+            
+            Cmbil.Text = "";
+            Cmbilce.Text = "";
+            TxtAd.Focus();
+        }
+      
+        private void FrmFirmalar_Load(object sender, EventArgs e)
+        {
+            firmalistesi();
+            sehirListesi();
+            carikodaciklamalar();
+            temizle();
+        }
+        
+        private void gridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            DataRow dr = gridView1.GetDataRow(gridView1.FocusedRowHandle);
+            if (dr != null)
+            {
+                Txtid.Text = dr["ID"].ToString();
+                TxtAd.Text = dr["AD"].ToString();
+                TxtYetkiliGorev.Text = dr["YETKILISTATU"].ToString();
+                TxtYetkili.Text = dr["YETKILIADSOYAD"].ToString();
+                MskYetkiliTC.Text = dr["YETKILITC"].ToString();
+                TxtSektor.Text = dr["SEKTOR"].ToString();
+                MskTelefon1.Text = dr["TELEFON1"].ToString();
+                MskTelefon2.Text = dr["TELEFON2"].ToString();
+                MskTelefon3.Text = dr["TELEFON3"].ToString();
+                TxtMail.Text = dr["MAIL"].ToString();
+                MskFax.Text = dr["FAX"].ToString();
+                Cmbil.Text = dr["IL"].ToString();
+                Cmbilce.Text = dr["ILCE"].ToString();
+                TxtVergi.Text = dr["VERGIDAIRE"].ToString();
+                RchAdres.Text = dr["ADRES"].ToString();
+                TxtKod1.Text = dr["OZELKOD1"].ToString();
+                TxtKod2.Text = dr["OZELKOD2"].ToString();
+                TxtKod3.Text = dr["OZELKOD3"].ToString();
+
+            }
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("insert into TBL_FIRMALAR(AD,YETKILISTATU,YETKILIADSOYAD,YETKILITC,SEKTOR,TELEFON1,TELEFON2,TELEFON3,MAIL,FAX,IL,ILCE,VERGIDAIRE,ADRES,OZELKOD1,OZELKOD2,OZELKOD3)values(@p1,@p2,@p3,@p4,@p5,@p6,@p7,@p8,@p9,@p10,@p11,@p12,@p13,@p14,@p15,@p16,@p17)", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", TxtAd.Text);
+            komut.Parameters.AddWithValue("@p2", TxtYetkiliGorev.Text);
+            komut.Parameters.AddWithValue("@p3", TxtYetkili.Text);
+            komut.Parameters.AddWithValue("@p4", MskYetkiliTC.Text);
+            komut.Parameters.AddWithValue("@p5", TxtSektor.Text);
+            komut.Parameters.AddWithValue("@p6", MskTelefon1.Text);
+            komut.Parameters.AddWithValue("@p7", MskTelefon2.Text);
+            komut.Parameters.AddWithValue("@p8", MskTelefon3.Text);
+            komut.Parameters.AddWithValue("@p9", TxtMail.Text);
+            komut.Parameters.AddWithValue("@p10", MskFax.Text);
+            komut.Parameters.AddWithValue("@p11", Cmbil.Text);
+            komut.Parameters.AddWithValue("@p12", Cmbilce.Text);
+            komut.Parameters.AddWithValue("@p13", TxtVergi.Text);
+            komut.Parameters.AddWithValue("@p14", RchAdres.Text);
+            komut.Parameters.AddWithValue("@p15", TxtKod1.Text);
+            komut.Parameters.AddWithValue("@p16", TxtKod2.Text);
+            komut.Parameters.AddWithValue("@p17", TxtKod3.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Firma Sisteme Eklendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            firmalistesi();
+            temizle();
+        }
+
+        private void Cmbil_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Cmbilce.Properties.Items.Clear();
+            SqlCommand komut = new SqlCommand("Select ILCE From TBL_ILCELER Where SEHIR=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", Cmbil.SelectedIndex + 1);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                Cmbilce.Properties.Items.Add(dr[0]);
+
+            }
+            bgl.baglanti().Close();
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("Delete From TBL_FIRMALAR where ID=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", Txtid.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            firmalistesi();
+            MessageBox.Show("Firma silindi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            temizle();
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("update TBL_FIRMALAR set AD=@p1,YETKILISTATU=@p2,YETKILIADSOYAD=@p3,YETKILITC=@p4,SEKTOR=@p5,TELEFON1=@p6,TELEFON2=@p7,TELEFON3=@p8,MAIL=@p9,FAX=@p10,IL=@p11,ILCE=@p12,VERGIDAIRE=@p13,ADRES=@p14,OZELKOD1=@p15,OZELKOD2=@p16,OZELKOD3=@p17 where ID=@p18", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", TxtAd.Text);
+            komut.Parameters.AddWithValue("@p2", TxtYetkiliGorev.Text);
+            komut.Parameters.AddWithValue("@p3", TxtYetkili.Text);
+            komut.Parameters.AddWithValue("@p4", MskYetkiliTC.Text);
+            komut.Parameters.AddWithValue("@p5", TxtSektor.Text);
+            komut.Parameters.AddWithValue("@p6", MskTelefon1.Text);
+            komut.Parameters.AddWithValue("@p7", MskTelefon2.Text);
+            komut.Parameters.AddWithValue("@p8", MskTelefon3.Text);
+            komut.Parameters.AddWithValue("@p9", TxtMail.Text);
+            komut.Parameters.AddWithValue("@p10", MskFax.Text);
+            komut.Parameters.AddWithValue("@p11", Cmbil.Text);
+            komut.Parameters.AddWithValue("@p12", Cmbilce.Text);
+            komut.Parameters.AddWithValue("@p13", TxtVergi.Text);
+            komut.Parameters.AddWithValue("@p14", RchAdres.Text);
+            komut.Parameters.AddWithValue("@p15", TxtKod1.Text);
+            komut.Parameters.AddWithValue("@p16", TxtKod2.Text);
+            komut.Parameters.AddWithValue("@p17", TxtKod3.Text);
+            komut.Parameters.AddWithValue("@p18", Txtid.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Firma Bilgileri Güncellendi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            firmalistesi();
+            temizle();
+            
+        }
+
+        private void BtnTemizle_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+    }
+}
